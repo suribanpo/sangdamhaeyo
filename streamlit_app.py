030d561c266e2981df595c3985d28c6b296a5368
suribanpo <162990981+suribanpo@users.noreply.github.com>
2025-02-05 15:22:32 +0000

    add : seaborn df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import seaborn as sns
+
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+df = sns.load_dataset("penguins")
+df
